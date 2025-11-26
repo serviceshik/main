8c5e1b2870f40ad3944f60e15cd7ccc3ea2e8cc5
Ali n <faure7n@gmail.com>
2025-11-26 19:18:00 +0330

    Fix product images

--- src/components/ProductInformationCard.jsx
+++ src/components/ProductInformationCard.jsx
@@ -1,33 +1,44 @@
-import React, { useState } from 'react'
-import "./ProductInformationCard.css"
+import React, { useState } from 'react';
+import "./ProductInformationCard.css";
 
 const ProductInformationCard = ({ title, image, description, price, isPair }) => {
-
     const [showFullImage, setShowFullImage] = useState(false);
+
+    const imageSrc = `/main/productImages/${image}`;
 
     return (
         <>
             <div className='product-information-container'>
                 <h2>{title}</h2>
                 <div className="product-image-box">
-                    <img src={image} className='product-image'
-                        onClick={() => setShowFullImage(true)} />
+                    <img
+                        src={imageSrc}
+                        className='product-image'
+                        onClick={() => setShowFullImage(true)}
+                        alt={title}
+                    />
                 </div>
 
-                <p className={title == 'مدل تزیین سرویس' ? 'sorted' : ''}>{description}</p>
+                <p className={title === 'مدل تزیین سرویس' ? 'sorted' : ''}>{description}</p>
 
                 {price
                     ? <span>{Number(price).toLocaleString('fa-IR')} تومان</span>
                     : <span>قیمت در توضیحات</span>}
+
                 {isPair && <div className='is-pair'>قیمت برای یک جفت</div>}
             </div>
+
             {showFullImage && (
                 <div className="fullscreen-overlay" onClick={() => setShowFullImage(false)}>
-                    <img src={`/main/productImages/${image}`} />
+                    <img
+                        src={imageSrc}
+                        className="fullscreen-image"
+                        alt={title}
+                    />
                 </div>
             )}
         </>
-    )
-}
+    );
+};
 
-export default ProductInformationCard
+export default ProductInformationCard;
