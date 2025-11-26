f510b83e043d1c3057a20bd92d7d63b491256338
Ali n <faure7n@gmail.com>
2025-11-26 19:57:04 +0330

    test

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function Home() {
       <h1 className='unselectable'>سرویس شیک</h1>
       <div>
         <p className='unselectable'>به سرویس شیک خوش آمدید</p>
-        <button onClick={() => navigate('/categories')}>مشاهده قیمت ها</button>
+        <button onClick={() => navigate('/categories')}>دیدن قیمت ها</button>
         <a target='_blank' href="https://www.instagram.com/service_shik/">
           <img src='/main/serviceshiklogo.webp' alt="logo" />
         </a>
